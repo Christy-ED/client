d8f46ab520f20a8a3f6c763de778b07c82e0f2b7
Christy <Edgardchristianne@gmail.com>
2024-09-12 21:58:34 -0400

    first commit

---
+++ src/BottomRoad.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const BottomRoad = () => {
+  return (
+<div className='bottomRow'>BottomRoad
+    <div className='down'>
+        
+
+    </div>
+</div>
+
+  )
+}
+
+export default BottomRoad
