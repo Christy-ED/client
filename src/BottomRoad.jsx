ada0385266a31d750fed3df5e72a4ef432dcfd3b
Christy <Edgardchristianne@gmail.com>
2024-09-12 22:04:47 -0400

    2nd commit

--- src/BottomRoad.jsx
+++ src/BottomRoad.jsx
@@ -5,9 +5,12 @@ const BottomRoad = () => {
   return (
 <div className='bottomRow'>BottomRoad
     <div className='down'>
-        
+        <h3 className='down_title'>Down</h3>
+        <div className= 'down_value'>3</div>
 
     </div>
+
+    <div className=''></div>
 </div>
 
   )
